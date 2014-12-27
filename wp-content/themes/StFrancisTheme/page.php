b6b56d995f239c6eea195c8dfafc8663ca07ae1f
Alisen <alisenpaige@gmail.com>
2014-12-27 17:31:45 -0500

    adds themes

---
+++ wp-content/themes/StFrancisTheme/page.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ * Theme Page Section for our theme.
+ *
+ * @package ThemeGrill
+ * @subpackage Spacious
+ * @since Spacious 1.0
+ */
+?>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<?php get_header(); ?>
+
+	<?php do_action( 'spacious_before_body_content' ); ?>
+
+	<div id="primary">
+		<div id="content" class="clearfix">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				
+
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	
+	<?php spacious_sidebar_select(); ?>
+
+	<?php do_action( 'spacious_after_body_content' ); ?>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/spacious/footer.php
@@ -0,0 +1,75 @@
+<?php 
+/**
+ * Theme Footer Section for our theme.
+ * 
+ * Displays all of the footer section and closing of the #main div.
+ *
+ * @package ThemeGrill
+ * @subpackage Spacious
+ * @since Spacious 1.0
+ */
+?>
+
+		</div><!-- .inner-wrap -->
+	</div><!-- #main -->	
+	<?php do_action( 'spacious_before_footer' ); ?>
+		<footer id="colophon" class="clearfix">	
+			<?php get_sidebar( 'footer' ); ?>
+
+			<div class="mobile-footer">
+				<h4>St. Francis of Assisi</h4>
+				<p>PHONE: 
+					<a href="tel:813.681.9115">813.681.9115</a>
+				</p>
+				<p>FAX: 813.689.4148</p>
+				<p>EMAIL: 
+					<a href="mailto:sfaseffner@tampabay.rr.com">sfaseffner@tampabay.rr.com</a>
+				</p>
+				<p>ADDRESS: <a href="https://www.google.com/maps/preview?ie=UTF-8&fb=1&gl=us&cid=17512669239884125301&q=St+Francis+of+Assisi+Catholic+Church&ei=Xu30U7SXO6GziwL60YGwBw&sqi=2&ved=0CIgBEPwSMAo" target="blank">
+					4450 County Road 579 | Seffner, FL 33584
+					</a>
+				</p>
+				<p>MAILING ADDRESS: PO Box 1218 | Seffner, FL 33583</p>
+
+			</div>
+
+			<div class="footer-socket-wrapper clearfix">
+				<div class="inner-wrap">
+					<div class="footer-socket-area">
+						
+						<nav class="small-menu" class="clearfix">
+							<?php
+								if ( has_nav_menu( 'footer' ) ) {									
+										wp_nav_menu( array( 'theme_location' => 'footer',
+																 'depth'           => -1
+																 ) );
+								}
+							?>
+		    			</nav>
+					</div>
+				</div>
+			</div>			
+		</footer>
+		<a href="#masthead" id="scroll-up"></a>	
+	</div><!-- #page -->
+	<?php wp_footer(); ?>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+</body>
+</html>
